1ac52387cbf84ed0f6a916797330b666344ba200
sovinbirla <123283605+sovinbirla@users.noreply.github.com>
2024-04-28 04:27:47 -0500

    got boudning boxes from input videos

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,7 +2,8 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8x")
 
-result = model.track("input_videos/input_video.mp4", conf=0.2, save=True)
+model.predict("input_videos/input_video.mp4", conf=0.2, save=True)
+# result = model.track("input_videos/input_video.mp4", conf=0.2, save=True)
 # print(result)
 # print("boxes:")
 # for box in result[0].boxes:
