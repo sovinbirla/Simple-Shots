d8f931bbfde58d31cd2dc8be6f7e843f9d42d450
sovinbirla <123283605+sovinbirla@users.noreply.github.com>
2024-04-28 05:06:44 -0500

    tracking myself

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,8 +1,13 @@
 from ultralytics import YOLO
+import torch
 
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+# print("using device: %s" % device)
 model = YOLO("yolov8x")
 
-model.predict("input_videos/input_video.mp4", conf=0.2, save=True)
+model.predict("input_videos/test.mp4", conf=0.2, save=True)
 # result = model.track("input_videos/input_video.mp4", conf=0.2, save=True)
 # print(result)
 # print("boxes:")
